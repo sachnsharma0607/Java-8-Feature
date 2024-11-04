a130f41a04856e27f95e493cf417b699e6f56d3c
sachin <sachinorsharma@gmail.com>
2024-11-05 02:04:11 +0530

    Java_8_Example02 with one parameter example for LamdaExpression with out return type

---
+++ src/main/java/com/java8/Java_8_Example02.java
@@ -0,0 +1,20 @@
+package com.java8;
+
+public class Java_8_Example02 {
+public static void main(String[] args) {
+	Funcational_interfaceExample_withParameter i=(s)->{
+		System.out.println("Lenght of the String : "+s.length());
+	};
+	i.m1("sachin Sharma");
+	i.m1("Ankita Bhat");
+	 i=(s)->{
+		System.out.println("Lenght of the String : "+s.replaceAll("\\s+","_"));
+	};
+	i.m1("Ankita B Bhat");
+}
+}
+
+@FunctionalInterface
+interface Funcational_interfaceExample_withParameter{
+	public  void m1(String s);
+}
